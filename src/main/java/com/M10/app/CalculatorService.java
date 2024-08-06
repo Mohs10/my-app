10aaca46fd2758d38e474a8f43478d99cddee23e
Mohs10 Technologies <104430393+Mohs10@users.noreply.github.com>
2024-08-06 20:33:05 +0530

    Create CalculatorService.java

---
+++ src/main/java/com/M10/app/CalculatorService.java
@@ -0,0 +1,11 @@
+package com.M10.app;
+
+public class CalculatorService {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
